86e911213d239b408672987030214a0096093657
Gabriel Saenz <Gabriel.saenz.24@gmail.com>
2026-05-08 01:05:42 -0500

    Updated links for upload to AWS EC2

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/api/create-checkout-session', async (req, res) => {
         product_data: {
           name: item.name,
           description: item.latin,
-          images: [`http://localhost:3000${item.image}`],
+          images: [`https://plants.gabriel-saenz.org${item.image}`],
         },
         unit_amount: Math.round(item.price * 100), // Stripe uses cents
       },
@@ -38,8 +38,8 @@ app.post('/api/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3000/success.html',
-      cancel_url: 'http://localhost:3000/cancel.html',
+      success_url: 'https://plants.gabriel-saenz.org/success.html',
+      cancel_url: 'https://plants.gabriel-saenz.org/cancel.html',
     });
 
     res.json({ url: session.url });
